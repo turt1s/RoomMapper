410a1b26722138b683a1a06ba74e478401d1e0e6
Sunny <turtlesareme1230@gmail.com>
2026-08-25 12:38:01 -0400

    moving to github

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // We are forcing the brain to use Pin 13 specifically
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(200);             // Fast 0.2 second delay
+  digitalWrite(13, LOW);
+  delay(200);             // Fast 0.2 second delay
+}
